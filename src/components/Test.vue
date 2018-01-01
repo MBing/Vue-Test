ac147bebde904f850f08ea3a54fd21f9ddcb96f2
Martin Bing <bing.martin.photography@gmail.com>
2018-01-01 14:03:49 +0100

    Add testview structure with user and some directives

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="test">
-    <h1>Test</h1>
+    <h1>{{ title }}</h1>
+    <p>{{ user.firstName }}</p>
+    <p v-text="user.lastName"></p>
+    <p v-if="isActive">This User is Active.</p>
+    <ul>
+      <li v-for="item in items">{{ item.title }}</li>
+    </ul>
   </div>
 </template>
 
@@ -8,6 +14,27 @@
 <script>
   export default {
     name: 'Test',
+    data() {
+      return {
+        title: 'Hello World',
+        user: {
+          firstName: 'John',
+          lastName: 'Doe',
+        },
+        isActive: true,
+        items: [
+          {
+            title: 'Item One',
+          },
+          {
+            title: 'Item Two',
+          },
+          {
+            title: 'Item Three',
+          },
+        ],
+      };
+    },
   };
 </script>
 
