6d0126ccaf271c8856fabf5bf312d9997be4645c
Martin Bing <bing.martin.photography@gmail.com>
2018-01-01 14:15:33 +0100

    Add some events with handlers

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="test">
-    <input v-model="title"><br>
+    <input v-model="title">
+    <br />
+
     <h1>{{ title }}</h1>
+
     <p>{{ user.firstName }}</p>
     <p v-text="user.lastName"></p>
     <p v-if="isActive">This User is Active.</p>
+
     <ul>
       <li v-for="item in items">{{ item.title }}</li>
     </ul>
+
+    <button v-on:click="greet(user.firstName)">Greeting</button>
+
+    <br />
+
+    <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="pressEnter">
   </div>
 </template>
 
@@ -35,6 +45,17 @@
           },
         ],
       };
+    },
+    methods: {
+      greet: (userName) => {
+        alert(`Hello ${userName}`);
+      },
+      pressKey: (e) => {
+        console.log(e.target.value);
+      },
+      pressEnter: () => {
+        console.log('You hit enter!');
+      },
     },
   };
 </script>
